7828669256b747774bd8c830863475bffaa1670f
etorth <anhonghe@gmail.com>
2026-04-05 08:31:42 -0700

    add new test cpp file

---
+++ test/test_mem_var_update.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <map>
+#include <string>
+#include <tuple>
+#include <map>
+#include <unordered_map>
+
+struct Test
+{
+    int a = 12;
+    float b = 23.2;
+    long c = 1234567890123456789L;
+    std::string d = "Hello, C++26!";
+    std::map<int, std::string> e
+    {
+        {1, "one"},
+        {2, "two"},
+        {3, "three"}
+    };
+
+    std::unordered_map<std::string, int> f
+    {
+        {"apple", 1},
+        {"banana", 2},
+        {"cherry", 3}
+    };
+};
+
+struct TestWrapper
+{
+    Test test {};
+    std::vector<int> vec{1, 2, 3, 4, 5};
+    std::tuple<int, float, std::string> tup{42, 3.14f, "tuple"};
+};
+
+int main()
+{
+    TestWrapper w {};
+
+    w.test.a = 20;
+    w.test.b = 45.6f;
+    w.test.d = "Updated string";
+
+    return 0;
+}
