b2c227c495d4c987be82112be9bc59957f293505
etorth <anhonghe@gmail.com>
2026-05-05 21:45:58 -0700

    update test

--- test/test_namespace.cpp
+++ test/test_namespace.cpp
@@ -1,28 +1,31 @@
 #include <string>
 
-int g_count = 0;
+int g_strcnt = 0;
+int g_intcnt = 0;
+
 namespace
 {
     struct A
     {
         int a;
         std::string s;
-        A(): a(g_count++), s(std::to_string(a)) {}
+        A(): a(g_strcnt++), s(std::to_string(a)) {}
     };
 }
+
 
 int main()
 {
     A a{};
-    int b = 10;
+    int b = g_intcnt++;
 
     {
         A a{};
-        int b = 10;
+        int b = g_intcnt++;
 
         {
             A a{};
-            int b = 10;
+            int b = g_intcnt++;
         }
 
         int c = 10;
